bd067bf1e2f186a5e6fd302723b5595451043c70
mobal <the.mobal@gmail.com>
2020-07-15 10:14:46 +0200

    Produces plain text instead of json

--- src/main/kotlin/hu/netcode/devtools/controller/WelcomeController.kt
+++ src/main/kotlin/hu/netcode/devtools/controller/WelcomeController.kt
@@ -1,16 +1,12 @@
 package hu.netcode.devtools.controller
 
 import org.slf4j.LoggerFactory
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(
-    produces = [MediaType.APPLICATION_JSON_VALUE],
-    value = ["/"]
-)
+@RequestMapping(value = ["/"])
 class WelcomeController {
     private val logger = LoggerFactory.getLogger(javaClass)
 
