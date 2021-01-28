a2ef48460850549397e7c0d1ffbd676b76887d40
mobal <info@netcode.hu>
2021-01-28 20:44:28 +0100

    Added WelcomeController tests

---
+++ src/test/kotlin/hu/netcode/devtools/controller/WelcomeControllerTest.kt
@@ -0,0 +1,42 @@
+package hu.netcode.devtools.controller
+
+import hu.netcode.devtools.service.ExceptionService
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@AutoConfigureMockMvc
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
+@WebMvcTest(
+    value = [
+        ExceptionService::class,
+        WelcomeController::class
+    ]
+)
+class WelcomeControllerTest {
+    @Autowired
+    private lateinit var exceptionService: ExceptionService
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @DisplayName(value = "WelcomeController: Tests for function welcome")
+    @Nested
+    @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
+    inner class Welcome() {
+        @Test
+        fun `successfully get welcome page`() {
+            mockMvc.get("/") {
+                accept = MediaType.TEXT_PLAIN
+            }.andExpect {
+                status { isOk() }
+            }
+        }
+    }
+}
